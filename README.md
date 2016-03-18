4134d5ddc90e52d47ca6937618376e1b02cb2f56
ouzhigang <ouzhiganggba@gmail.com>
2016-03-19 02:24:27 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+guide
+=====
+
+用ViewPager实现欢迎引导页面 . 
+
+
+修改自https://github.com/leigo/guide
---
+++ app/src/main/java/com/kobo3/ozgguide/adapter/ViewPagerAdapter.java
@@ -0,0 +1,112 @@
+package com.kobo3.ozgguide.adapter;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.kobo3.ozgguide.R;
+
+public class ViewPagerAdapter extends PagerAdapter {
+
+	// 界面列表
+	private List<View> views;
+	private Activity activity;
+
+	private static final String SHAREDPREFERENCES_NAME = "first_pref";
+
+	public ViewPagerAdapter(List<View> views, Activity activity) {
+		this.views = views;
+		this.activity = activity;
+	}
+
+	// 销毁arg1位置的界面
+	@Override
+	public void destroyItem(ViewGroup container, int position, Object object) {
+		container.removeView(views.get(position));
+	}
+
+	@Override
+	public void finishUpdate(ViewGroup container) {
+	}
+
+	// 获得当前界面数
+	@Override
+	public int getCount() {
+		if (views != null) {
+			return views.size();
+		}
+		return 0;
+	}
+
+	// 初始化arg1位置的界面
+	@Override
+	public Object instantiateItem(ViewGroup container, int position) {
+		container.addView(views.get(position), 0);
+		if (position == views.size() - 1) {
+			ImageView mStartWeiboImageButton = (ImageView)container.findViewById(R.id.iv_start_weibo);
+			mStartWeiboImageButton.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					// 设置已经引导
+					setGuided();
+					goHome();
+
+				}
+
+			});
+		}
+		return views.get(position);
+	}
+
+	private void goHome() {
+		// 跳转
+		Intent intent = new Intent(activity, com.kobo3.ozgguide.MainActivity.class);
+		activity.startActivity(intent);
+		activity.finish();
+	}
+
+	/**
+	 * 
+	 * method desc：设置已经引导过了，下次启动不用再次引导
+	 */
+	private void setGuided() {
+		SharedPreferences preferences = activity.getSharedPreferences(SHAREDPREFERENCES_NAME, Context.MODE_PRIVATE);
+		Editor editor = preferences.edit();
+		// 存入数据
+		editor.putBoolean("isFirstIn", false);
+		// 提交修改
+		editor.commit();
+	}
+
+	// 判断是否由对象生成界面
+	@Override
+	public boolean isViewFromObject(View view, Object object) {
+		return (view == object);
+	}
+
+	@Override
+	public void restoreState(Parcelable state, ClassLoader loader) {
+	}
+
+	@Override
+	public Parcelable saveState() {
+		return null;
+	}
+
+	@Override
+	public void startUpdate(ViewGroup container) {
+	}
+
+}
